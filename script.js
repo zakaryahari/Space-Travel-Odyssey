b60fe6d85c2e798e5871d3dfe59632225f3b3782
zakarya hari <zakariahari42@gmail.com>
2025-11-12 04:07:22 +0100

    Ajouter la founction de calculer le price , n'est pas terminer.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let destinations_data = [];
 let spacecraft_data = [];
 let booking_option_data = [];
 
+
 const SESSION_KEY = 'login';
 const VALID_EMAIL = 'zakaryahari42@gmail.com';
 
@@ -132,19 +133,20 @@ function Load_Destination_option() {
         default_option.textContent = 'Please select your destination...';
         default_option.selected = true ;
         default_option.setAttribute('value','default_option');
-
+        
         destination_select_input.appendChild(default_option);
-
+        
         destinations_data.forEach(Destination_val => {
             const child_option = document.createElement('option');
             child_option.textContent = Destination_val.name;
             // console.log(Destination_val.name);
             // if (Destination_val.id === "moon") {
-            //     child_option.selected = true;
-            // }
-            child_option.setAttribute('value', Destination_val.id);
-
-            destination_select_input.appendChild(child_option);
+                //     child_option.selected = true;
+                // }
+                child_option.setAttribute('value', Destination_val.id);
+                child_option.setAttribute('price_val',Destination_val.price);
+                
+                destination_select_input.appendChild(child_option);
         });
     }
 }
@@ -162,7 +164,7 @@ function Afficher_Accommodation(List) {
                     const accomodation_card = `
                             <label
                                 class="flex flex-col p-4 border border-neon-blue/30 rounded-lg cursor-pointer transition-all hover:bg-space-blue/70 has-checked:border-neon-blue">
-                                <input type="radio" name="accommodation" value="${acc.id}" class="radio_btn hidden peer">
+                                <input type="radio" name="accommodation" value="${acc.id}" class="radio_btn hidden peer" price_val="${acc.pricePerDay}">
                                 <span class="font-orbitron text-lg text-neon-blue peer-checked:text-neon-cyan">${acc.name}</span>
                                 <p class="text-gray-400 text-sm">${acc.shortDescription}</p>
                             </label>
@@ -175,18 +177,21 @@ function Afficher_Accommodation(List) {
 }
 
 const destination = document.getElementById('destination');
+const totalprice_input = document.getElementById('total-price');
 
 if (destination) {
+    
     destination.addEventListener('change', (e) => {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         let selected_destinations = [];
         destinations_data.forEach(destinations_val => {
             if (e.target.value === destinations_val.id) {
                 selected_destination = destinations_val;
                 Afficher_Accommodation(selected_destination.accommodations);
             }
-        })
+        });
     });
+    Calculat_price();
 }
 
 function Passenger_inputset_load_inputs(val) {
@@ -231,21 +236,85 @@ function Passenger_inputset_load_inputs(val) {
                 Passenger_inputset_container.innerHTML += inner_inputset;
             }
 }
+
+
+function Passenger_inputset_load_inputs_single(val) {
+
+                const inner_inputset = `
+                    <h3 class="font-orbitron text-2xl font-bold mb-4 mt-8 border-b border-neon-blue/20 pb-2">Personal
+                            Information (Passenger N°${val+1})</h3>
+                        <div class="grid grid-cols-1 md:grid-cols-2 gap-6" id="passenger-container">
+                            <div class="form-group">
+                                <label for="first-name-1" class="block mb-2 font-semibold text-gray-200">First Name</label>
+                                <input type="text" id="first-name-1" name="firstName[]" required
+                                    placeholder="Enter your first name"
+                                    class="w-full p-3 rounded-lg bg-space-blue border border-neon-blue/30 text-white focus:border-neon-blue focus:outline-none transition-colors">
+                            </div>
+                            <div class="form-group">
+                                <label for="last-name-1" class="block mb-2 font-semibold text-gray-200">Last Name</label>
+                                <input type="text" id="last-name-1" name="lastName[]" required
+                                    placeholder="Enter your last name"
+                                    class="w-full p-3 rounded-lg bg-space-blue border border-neon-blue/30 text-white focus:border-neon-blue focus:outline-none transition-colors">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="email-1" class="block mb-2 font-semibold text-gray-200">Email Address</label>
+                                <input type="email" id="email-1" name="email[]" required placeholder="Enter your email"
+                                    class="w-full p-3 rounded-lg bg-space-blue border border-neon-blue/30 text-white focus:border-neon-blue focus:outline-none transition-colors">
+                            </div>
+                            <div class="form-group">
+                                <label for="phone-1" class="block mb-2 font-semibold text-gray-200">Phone Number</label>
+                                <input type="tel" id="phone-1" name="phone[]" required placeholder="Enter your phone number"
+                                    class="w-full p-3 rounded-lg bg-space-blue border border-neon-blue/30 text-white focus:border-neon-blue focus:outline-none transition-colors">
+                            </div>
+
+                            <div class="col-span-2">
+                                <label for="requirements" class="block mb-2 font-semibold text-gray-200">Special
+                                    Requirements</label>
+                                <textarea id="requirements" name="requirements"
+                                    placeholder="Any special requirements or notes..."
+                                    class="w-full p-3 rounded-lg bg-space-blue border border-neon-blue/30 text-white focus:border-neon-blue focus:outline-none transition-colors min-h-[100px]"></textarea>
+                            </div>
+                        </div>
+                `;
+                Passenger_inputset_container.innerHTML += inner_inputset;
+}
+
 const Passenger_inputset_container = document.getElementById('Passenger_inputset_container');
 const add_passenger_btn = document.getElementById('add_passenger_btn');
+// const add_passenger_btn = document.getElementById('add_passenger_btn');
 
 function Passenger_inputset(val) {
+ 
     if (val == 1 || val == 2 || val == 3) {
         if (Passenger_inputset_container) {
+
             Passenger_inputset_container.innerHTML = '';
             add_passenger_btn.classList.add('hidden');
+
+
             if (val == 3) {
                 add_passenger_btn.classList.remove('hidden');
                 // Passenger_inputset_load_inputs(val);
             }
             Passenger_inputset_load_inputs(val);
         }
+    let add_passenger_counter = 0;
+
     }
+}
+
+// function Add_multi_passenger() {
+    
+// }
+let add_passenger_counter = 3;
+let radio_option ;
+
+if (add_passenger_btn) {
+    
+    add_passenger_btn.addEventListener('click' , ()=>{
+        Passenger_inputset_load_inputs_single(add_passenger_counter);        
+    });
 
 }
 
@@ -256,11 +325,71 @@ if (radio_container) {
         if (e.target.tagName === 'INPUT' && e.target.type === 'radio') {
             // console.log('TEST');
             const radio_selected = e.target.value;
+            radio_option = e.target.value;
             // console.log(radio_selected);
             Passenger_inputset(radio_selected);
+            Calculat_price();
         }
+
+    });
+    
+}
+
+if (radio_container) {
+    Passenger_inputset_load_inputs_single(0);
+}
+
+const accommodationContainer = document.getElementById('Accommodation_container_id');
+
+if (accommodationContainer) {
+    accommodationContainer.addEventListener('change', () => { 
+        Calculat_price();
     });
 }
+
+function findDataById(data_arr , selected_id) {
+    return data_arr.find(item => item.id === selected_id);
+}
+
+function Calculat_price() {
+    let Total_price = 0;
+
+    const destination_select_input = document.getElementById('destination');
+    // const radio_container = document.getElementById('radio_container');
+    const totalpriceInput = document.getElementById('total-price');
+
+    const checkedInput = document.querySelector(`input[name="accommodation"]:checked`);
+    const radio_option = document.querySelector(`input[name="passengers"]:checked`);
+
+    if (destination_select_input === 'default_option' || !destination_select_input || !checkedInput || !radio_option) {
+        totalpriceInput.textContent = '$0.00';
+        return;
+    }
+
+    const destObject = findDataById(destinations_data, selectedDestId);
+    const accommObject = findDataById(accommodation_data, selectedAccommId);
+
+    if (!destObject || !accommObject) {
+        totalpriceInput.textContent = '$0.00 (Data Error)';
+        return;
+    }
+
+    Total_price += destObject.price;
+
+    Total_price += accommObject.pricePerDay;
+
+    Total_price *= radio_option.value; 
+
+    totalpriceInput.textContent = `$${Total_price}.00`;
+
+
+    // const optionObject = booking_option_data.passengerOptions.find(opt => opt.value === selectedPassengerOptionId);
+
+
+    // console.log(checkedInput);
+    
+}
+
 
 // booking js part
 
